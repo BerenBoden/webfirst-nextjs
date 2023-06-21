5a277bbe4f8abcf7b89dbea631b68eaf469aa792
berenboden <beren.boden@gmail.com>
2023-06-21 16:49:42 +1200

    update

--- app/components/common/Footer.tsx
+++ app/components/common/Footer.tsx
@@ -221,7 +221,7 @@ export default function Footer() {
             ))}
           </div>
           <p className="mt-8 text-xs leading-5 text-gray-500 md:order-1 md:mt-0">
-            &copy; 2020 Your Company, Inc. All rights reserved.
+            &copy; 2023 webfirst.co.nz, Inc. All rights reserved.
           </p>
         </div>
       </div>
